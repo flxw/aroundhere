057b6e832e8d5b58ee9cdc4ca62167aac1deca88
Markus <Markus@Markuss-MacBook-Pro.local>
2014-10-30 10:56:13 +0100

    Marker addable to Map, with pop-up description

--- public/js/map.js
+++ public/js/map.js
@@ -1,12 +1,37 @@
+var map = null
+
 function initialize()
 {
-var mapProp = {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
-  mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-var map=new google.maps.Map(document.getElementById("googleMap")
-  ,mapProp);
+	var mapProp = {
+	  center:new google.maps.LatLng(52.521918,13.413215),
+	  zoom:11,
+	  mapTypeId:google.maps.MapTypeId.ROADMAP
+	  }
+
+	map = new google.maps.Map(document.getElementById("googleMap")
+	  ,mapProp)
+
+	addMarker(52.521918, 13.413215, "Alexanderplatz");
+}
+
+function addMarker(latitude, longitude, description){
+	var pos =new google.maps.LatLng(latitude, longitude);
+
+	var marker=new google.maps.Marker({
+  		position: pos
+  	});
+
+	marker.setMap(map);
+
+	var infowindow = new google.maps.InfoWindow({
+  		content: description
+  	});
+
+	google.maps.event.addListener(marker,'click',function() {
+  		infowindow.open(map,marker);
+  	});
+
+	
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
