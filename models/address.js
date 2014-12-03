fa9760c05b3f927b789ec72aaeffd37250f2a9f9
Markus <Markus@Markuss-MacBook-Pro-3.local>
2014-12-03 15:55:46 +0100

    updated model

--- models/address.js
+++ models/address.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var addressSchema = new mongoose.Schema({
   geolocation: {
     type: { type: String, default: 'Point' },
-    coordinates: [Number],
+    coordinates: {type:[Number], index '2d'}
   },
   formatted: String,
   belongsToMonument: { type: mongoose.Schema.Types.ObjectId, ref: 'Monument' }
