d9fff5ecf4d66f75d04929424ab94949a0ed26f2
Felix Wolff <felix.stefan.wolff@gmail.com>
2014-12-13 19:14:03 +0100

    Manage google map view data from a service

---
+++ public/js/services/mapservice.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('aroundhere').factory('MapService', [function() {
+  var currentPositionMarker = null
+  var map = null
+
+  // initialization
+  var mapProp = {
+    center: new google.maps.LatLng(52.521918,13.413215),
+    zoom: 17,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+
+  map = new google.maps.Map(document.getElementById('google-map'), mapProp)
+
+  // methods
+  function setCurrentPosition(latitude, longitude) {
+    var oldMarker = currentPositionMarker
+    var pos = new google.maps.LatLng(latitude, longitude)
+
+    currentPositionMarker = new google.maps.Marker({
+      position: pos,
+      map: map
+    })
+
+    if (oldMarker === null) {
+      map.setCenter(pos)
+    } else {
+      oldMarker.setMap(null)
+    }
+  }
+
+ return {
+  setCurrentPosition: setCurrentPosition
+ }
+}])
