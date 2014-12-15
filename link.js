b2d16ff6e9a82d7a523161a58c938a9cfa38ddc9
Markus Petrykowski <git@markus-petrykowski.de>
2014-12-15 21:50:55 +0100

    Added Wikidata Linkparsing

--- link.js
+++ link.js
@@ -16,7 +16,104 @@ var wikiLinks = [
   "http://de.wikipedia.org/wiki/Liste_der_Kulturdenkmale_in_Berlin-Mitte/Oranienburger_Vorstadt",
   "http://de.wikipedia.org/wiki/Liste_der_Kulturdenkmale_in_Berlin-Mitte/Rosenthaler_Vorstadt",
   "http://de.wikipedia.org/wiki/Liste_der_Kulturdenkmale_in_Berlin-Mitte/Spandauer_Vorstadt",
-  "http://de.wikipedia.org/wiki/Liste_der_Kulturdenkmale_in_Berlin-Mitte/Stralauer_Vorstadt"
+  "http://de.wikipedia.org/wiki/Liste_der_Kulturdenkmale_in_Berlin-Mitte/Stralauer_Vorstadt",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Adlershof",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Alt-Hohensch%C3%B6nhausen",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Alt-Treptow",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Altglienicke",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Baumschulenweg",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Biesdorf",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Blankenburg",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Blankenfelde",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Bohnsdorf",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Borsigwalde",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Britz",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Buch",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Buckow",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Charlottenburg",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Charlottenburg-Nord",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Dahlem",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Falkenberg",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Falkenhagener_Feld",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Fennpfuhl",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Franz%C3%B6sisch_Buchholz",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Friedenau",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Friedrichsfelde",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Friedrichshagen",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Friedrichshain",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Frohnau",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Gatow",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Gesundbrunnen",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Gropiusstadt",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Gr%C3%BCnau",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Grunewald",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Hakenfelde",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Halensee",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Hansaviertel",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Haselhorst",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Heiligensee",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Heinersdorf",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Hellersdorf",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Hermsdorf",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Johannisthal",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Karlshorst",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Karow",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Kaulsdorf",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Kladow",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Konradsh%C3%B6he",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-K%C3%B6penick",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Kreuzberg",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Lankwitz",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Lichtenberg",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Lichtenrade",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Lichterfelde",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-L%C3%BCbars",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Mahlsdorf",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Malchow",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Mariendorf",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Marienfelde",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-M%C3%A4rkisches_Viertel",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Marzahn",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Mitte",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Moabit",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-M%C3%BCggelheim",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Neu-Hohensch%C3%B6nhausen",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Neuk%C3%B6lln",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Niedersch%C3%B6neweide",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Niedersch%C3%B6nhausen",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Nikolassee",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Obersch%C3%B6neweide",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Pankow",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Pl%C3%A4nterwald",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Prenzlauer_Berg",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Rahnsdorf",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Reinickendorf",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Rosenthal",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Rudow",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Rummelsburg",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Schmargendorf",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Schm%C3%B6ckwitz",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Sch%C3%B6neberg",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Siemensstadt",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Spandau",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Staaken",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Steglitz",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Tegel",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Tempelhof",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Tiergarten",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Waidmannslust",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Wannsee",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Wartenberg",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Wedding",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Wei%C3%9Fensee",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Westend",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Wilhelmsruh",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Wilhelmstadt",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Wilmersdorf",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Wittenau",
+  //"http://de.wikipedia.org//wiki/Liste_der_Kulturdenkmale_in_Berlin-Zehlendorf"
+
+
 ]
 
 var linkData = function(id, topCallback){
@@ -43,23 +140,38 @@ function crawlURL(url, callback, id, topCallback){
   });
 }
 
+//Regular Expressions for parsing Wikipedia
 var regIds  = /<td rowspan.*id="\d{8}/g
 var regImages = /src=".*\.jpg"/gi
+var possibleLink = /<td[\s\S]*?<\/td>/
+var linkExtract = /\/.*?" /
 
 function getLinkForId(id, url, html, topCallback){
     var index = html.search(id)
+    var linkedData = {}
 
     if(index > -1) {
+      //Define Range of interesting Part for a monumentID
       var startIndex = html.indexOf("href", index)
-
-      var endIndex = html.indexOf('\"', startIndex + 6)
-
-      var link = html.substring(startIndex + 6, endIndex)
-
       var endScopeImg = html.search(getNextId(id, html))
-      var imgLinks = getImageForId(id, html.slice(startIndex, endScopeImg))
 
-      topCallback({link: link, images: imgLinks})
+      var monumentHtml = html.slice(startIndex, endScopeImg)
+
+      //EndIndex for getting the SenatBerlin link
+      var endIndex = html.indexOf('\"', startIndex + 6)
+      var link = html.substring(startIndex + 6, endIndex)
+      linkedData.link = link
+
+      //Parse Images from Wikipedia
+      var imgLinks = getImageForId(id, monumentHtml)
+      linkedData.images = imgLinks
+
+      //Parse for possible Wikidata Link
+      var wikiDataLink = getLinkFromHtml(monumentHtml)
+      if(wikiDataLink)
+        linkedData.wikiDataLink = wikiDataLink
+
+      topCallback(linkedData)
     }
 
 
@@ -69,6 +181,21 @@ exports.linkData = linkData
 
 
 // private -------------------------------------------------------
+
+var getLinkFromHtml = function(html){
+  var nextColumn = html.match(possibleLink)[0]
+  console.log(nextColumn)
+  console.log(nextColumn.match(/a/).length)
+  if(nextColumn.match(/a/).length == 1){
+    var link = nextColumn.match(linkExtract)
+    link = link[0].slice(0, link.length-3)
+    link = "http://www.dbpedia.org" + link.replace("wiki", "page")
+    console.log(link)
+    return link
+  }
+
+  return null
+}
 
 var getImageForId = function(id, html){
   var images = html.match(regImages)
--- moduleRDF.js
+++ moduleRDF.js
@@ -1,6 +1,6 @@
 
 
-var rdfHeader =    "@prefix  rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n"      +
+var rdfHeader =    "@prefix  rdf:           <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n"      +
                    "@prefix  rdfs: 			<http://www.w3.org/2000/01/rdf-schema#> .\n"  +
                    "@prefix  s: 			<http://schema.org/address> .\n"              +
                    "@prefix  owl: 			<http://www.w3.org/2002/07/owl#> .\n"         +
@@ -13,23 +13,23 @@ var rdfBody = ""
 
 var createRDF = function(data){
     rdfBody = "<http://aroundhere.flxw.de/monument/"+ data._id + ">\n"
-    if(data.description != "")
+    if(data.description)
               rdfBody += "\trdfs:label " + data.description + "\n"
 
     if(data.addresses)
         data.addresses.forEach(function(address){
             console.log(address)
             rdfBody += "\tgeo:location [ \n"
-            rdfBody += "\t\ts:adress\t"+ address._id +"\n"
-            rdfBody += "\t\tgeo:lat \t" + address.geolocation.coordinates[0] + "\n"
-            rdfBody += "\t\tgeo:long\t" + address.geolocation.coordinates[1] + "\n"
-            rdfBody += "\t ] \n"
+            rdfBody += "\t\ts:adress\t"+ address._id +",\n"
+            rdfBody += "\t\tgeo:lat \t" + address.geolocation.coordinates[0] + ",\n"
+            rdfBody += "\t\tgeo:long\t" + address.geolocation.coordinates[1] + ",\n"
+            rdfBody += "\t ] ,\n"
         })
     data.images.forEach(function(image){
-        rdfBody += "\tdbo:thumbnail " + image + "\n"
+        rdfBody += "\tdbo:thumbnail <" + image + ">,\n"
     })
 
-    rdfBody += "\towl:sameAs <" + data.link + ">\n."
+    rdfBody += "\towl:sameAs <" + data.wikiDataLink + ">\n."
 
     return rdfHeader + rdfBody
 }
