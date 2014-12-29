2544745bcaae3c096a6ccdfcc6892b117c00df7a
Felix Wolff <felix.stefan.wolff@gmail.com>
2014-12-29 21:10:00 +0100

    Remove angular security disable

--- public/js/aroundhere.js
+++ public/js/aroundhere.js
@@ -2,12 +2,6 @@
 
 var app = angular.module('aroundhere', ['ngGeolocation', 'ngRoute'])
 
-app.config(function($sceProvider) {
-  // Completely disable SCE.  For demonstration purposes only!
-  // Do not use in new projects. (needed for core-image...)
-  $sceProvider.enabled(false);
-});
-
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/', {
