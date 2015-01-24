902f1d6a8c4ec95d07237e3871be4bb2693a4462
Felix Wolff <felix.stefan.wolff@gmail.com>
2015-01-24 19:42:22 +0100

    depart from old Angular.js APP

--- public/js/services/googleapiservice.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('aroundhere').factory('GoogleApiService', [function() {
- return {
-  staticMapServiceUrl: 'https://maps.googleapis.com/maps/api/staticmap',
-  key: 'AIzaSyAYfnpR4oWhEB6dnCb2Qr2puVPjsLt8j2I'
- }
-}])
